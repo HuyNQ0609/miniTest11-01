922a0212f1e633132a408a658abd7b514078eca3
HuyNQ0609 <nguyenhuy06092001@gmail.com>
2023-01-12 05:49:35 +0700

    Minitest 11-01

--- ex2-Product/src/Main.java
+++ ex2-Product/src/Main.java
@@ -2,36 +2,30 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        Product product1 = new Product("3a95bc", "iPhone ", 420000, 5, 12.5);
-        Product product2 = new Product("fg32k3", "Samsung", 430000, 6, 17.0);
-        Product product3 = new Product("4js5lx", "Realme ", 320000, 7, 13.0);
-        Product product4 = new Product("50sde6", "Xiaomi ", 410000, 5, 22.5);
-        Product product5 = new Product("e098w4", "Oppo   ", 230000, 9, 28.0);
-        Product[] array = {product1, product2, product3, product4, product5};
-        System.out.println(Arrays.toString(array));
+//        Scanner input = new Scanner(System.in);
+        Product[] products = new Product[5];
+        products[0] = new Product("e098w4", "Oppo   ", 230000, 9, 28.0);
+        products[1] = new Product("3a95bc", "iPhone ", 420000, 5, 12.5);
+        products[2] = new Product("fg32k3", "Samsung", 430000, 6, 17.0);
+        products[3] = new Product("4js5lx", "Realme ", 320000, 7, 13.0);
+        products[4] = new Product("50sde6", "Xiaomi ", 410000, 5, 22.5);
+        System.out.println(Arrays.toString(products));
 
-        double[][] arr = {  {product1.getTotalNoSale(), product1.getAfterSale()},
-                            {product2.getTotalNoSale(), product2.getAfterSale()},
-                            {product3.getTotalNoSale(), product3.getAfterSale()},
-                            {product4.getTotalNoSale(), product4.getAfterSale()},
-                            {product5.getTotalNoSale(), product5.getAfterSale()} };
-
-        double totalPriceDifference =  totalNoSale(arr)- totalPriceAfterSale(arr);
-        System.out.println("Total price after sale = " + totalPriceAfterSale(arr));
-        System.out.println("Total price difference after sale = " + totalPriceDifference);
+        System.out.println("Total price after sale = " + totalPriceAfterSale(products));
+        System.out.println("Total price difference after sale = " + (totalNoSale(products) - totalPriceAfterSale(products)));
     }
-    public static double totalPriceAfterSale(double[][] arr) {
+    public static double totalPriceAfterSale(Product[] arr) {
         double total = 0;
-        for (double[] doubles : arr) {
-            total += doubles[1];
+        for (Product i : arr) {
+            total += i.getAfterSale();
         }
-        return total;
+        return Math.round(total);
     }
-    public static double totalNoSale(double[][] arr) {
+    public static double totalNoSale(Product[] arr) {
         double sum = 0;
-        for (double[] doubles : arr) {
-            sum += doubles[0];
+        for (Product i : arr) {
+            sum += i.getTotalNoSale();
         }
-        return sum;
+        return Math.round(sum);
     }
 }
--- ex2-Product/src/Product.java
+++ ex2-Product/src/Product.java
@@ -1,10 +1,18 @@
+import java.util.Scanner;
+
 public class Product {
     private String codeSP;
     private String name;
     private int price;
     private int amount;
     private double sale;
-
+    private  Product() {
+        this.codeSP = "";
+        this.name = "";
+        this.price = 0;
+        this.amount = 0;
+        this.sale = 0;
+    }
     public Product(String codeSP, String name, int price, int amount, double sale) {
         this.codeSP = codeSP;
         this.name = name;
@@ -12,43 +20,33 @@ public class Product {
         this.amount = amount;
         this.sale = sale;
     }
-
     public String getCodeSP() {
         return codeSP;
     }
-
     public void setCodeSP(String codeSP) {
         this.codeSP = codeSP;
     }
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public int getPrice() {
         return price;
     }
-
     public void setPrice(int price) {
         this.price = price;
     }
-
     public int getAmount() {
         return amount;
     }
-
     public void setAmount(int amount) {
         this.amount = amount;
     }
-
     public double getSale() {
         return sale;
     }
-
     public void setSale(double sale) {
         this.sale = sale;
     }
@@ -56,9 +54,8 @@ public class Product {
         return price * amount;
     }
     public double getAfterSale() {
-        return price * amount * sale / 100;
+        return getTotalNoSale() * (100 - sale) / 100;
     }
-
     @Override
     public String toString() {
         return   "\n  "+ codeSP +"    " + name + "    " + price + "    " + amount + "    " + sale +"% " ;
